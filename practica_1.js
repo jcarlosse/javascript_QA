2462dea9b7342b5e868860ce73b1c2f39a7dffa7
jcarlosse <82538590+jcarlosse@users.noreply.github.com>
2021-04-16 21:20:39 -0500

    Primeras practicas

---
+++ practica_1.js
@@ -0,0 +1,24 @@
+// Imprimir en consola
+
+console.log('Aquí se imprime una linea')
+
+// Imprimir multiple lineas
+console.log("linea uno \n linea dos")
+
+// Como manejar un array en Javascript
+var myarray = [];
+myarray[3] = 67
+myarray[5] = 'Lap'
+console.log(myarray[0]);
+console.log(myarray[1]);
+console.log(myarray[2]);
+console.log(myarray[3]);
+console.log(myarray[4]);
+console.log(myarray[5]);
+
+// Agregar Elementos
+myarray.push(4)
+myarray.push('Fer')
+myarray.push(false)
+myarray[3] = 'Zapato'
+console.log(myarray)
