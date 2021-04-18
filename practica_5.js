18992ffe6c0aa36c4413af115f1f1b271e887b11
jcarlosse <82538590+jcarlosse@users.noreply.github.com>
2021-04-17 23:59:23 -0500

    subiendo practica 4 y 5

---
+++ practica_5.js
@@ -0,0 +1,19 @@
+// Objetos
+
+var myObj = {};
+
+var persona = {
+
+    nombre: "Juan Carlos",
+    apellido: "Suárez"
+}
+
+persona.edad = 46;
+//otra manera de agregarle propiedad
+persona["cabello"] = "Castaño"; //la propiedad con comillas
+persona["salario"] = 1000000;
+
+console.log(persona);
+
+
+
